bb1a53cafbb16e22c9287a1f348f03ef67408e66
Ebbe Johanssson <mr.ebbe.johansson@gmail.com>
2024-12-17 09:57:33 +0100

    Har gjort så bolle inte fastnar samt gjort att hastigheten ökar med spelets gång. Allt är klart men har ej gjort så bollen startar åt olika håll aka den blrjar alltid med att åka åt höger.

--- Ball.cs
+++ Ball.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using EasyMonoGame;
 using Microsoft.VisualBasic.FileIO;
@@ -11,7 +13,7 @@ namespace EasyDemo2
 {
     internal class Ball : Actor
     {
-        private float Speed = 1000;
+        private float Speed = 500;
 
         private int Lscore;
         private int Rscore;
@@ -42,9 +44,9 @@ namespace EasyDemo2
                     Rotation = -angle;
                     this.X = 750;
                     World.ShowText("angle: " + angle, 400, 500);
-                    if (Speed < 1000)
+                    if (Speed < 1750)
                     {
-                        Speed += 10;
+                        Speed += 50;
                     }
                 }
                 else
@@ -52,55 +54,35 @@ namespace EasyDemo2
                     Rotation = angle;
                     this.X = 50;
                     World.ShowText("angle: " + angle, 400, 500);
-                    if (Speed > 1000)
+                    if (Speed > -1750)
                     {
-                        Speed -= 10;
+                        Speed -= 50;
                     }
                 }
                 World.ShowText("speed: " + Speed, 400, 550);
                 Speed = -Speed;
                 Move(Speed * deltaTime);
             }
-            //                if (this.X > 400)
-            //                {
-            //                    Rotation = -angle + 180;
-            //                    //this.X = 600;
-            //                    Move(Speed * deltaTime * 5);
-            //                    Speed += 10;
-            //                    World.ShowText("angle: " + angle, 400, 500);
-            //                }
-            //                else
-            //                { 
-            //                
-            //                    if (-45 < angle && angle < 45)
-            //                    {
-            //                        Rotation = angle;
-            //                    }
-            //                    else
-            //                    {
-            //                        Rotation = 0;
-            //                    }
 
-            //                    this.X = 200;
-            //                    Speed += 10;
-            //                    World.ShowText("angle: " + angle, 400, 500);
-            //                }
-
-
-            //           }
 
 
 
             if (this.X > 800)
             {
                 Rscore += 1;
-                Speed = -Speed;
+                Speed = 500;
 
+                this.X = 400;
+                this.Y = 300;
             }
             if (this.X < 0)
             {
                 Lscore += 1;
-                Speed = -Speed;
+                Speed = 500;
+
+
+                this.X = 400;
+                this.Y = 300;
             }
             if (this.Y > 600)
             {
@@ -116,3 +98,4 @@ namespace EasyDemo2
         }
     }
 }
+
